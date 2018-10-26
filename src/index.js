20788d049ed65dd9bc519566c659859c994a6425
Shim Soyoung <sovv12@gmail.com>
2018-10-26 11:38:42 +0900

    a

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
-const form = document.querySelector('.number-list');
-let input1 = document.querySelector('.input1');
-let input2 = document.querySelector('.input2');
-let input3 = document.querySelector('.input3');
+const form = document.querySelector(".number-list");
+let input1 = document.querySelector(".input1");
+let input2 = document.querySelector(".input2");
+let input3 = document.querySelector(".input3");
 
-const randomNum  = () => {
+const randomNum = () => {
   const numberArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   // 랜덤으로 세자리 숫자 뽑기
-  const one = numberArr.splice(Math.floor(Math.random() * numberArr.length),1,0)
-  const two = numberArr.splice(Math.floor(Math.random() * numberArr.length),1,0)
-  const three = numberArr.splice(Math.floor(Math.random() * numberArr.length),1,0)
-  return one+two+three
-}
-randomNum()
+  const one = numberArr.splice(
+    Math.floor(Math.random() * numberArr.length),
+    1,
+    0
+  );
+  const two = numberArr.splice(
+    Math.floor(Math.random() * numberArr.length),
+    1,
+    0
+  );
+  const three = numberArr.splice(
+    Math.floor(Math.random() * numberArr.length),
+    1,
+    0
+  );
+  return one + two + three;
+};
+randomNum();
 
 // 값을 고정시키기 위해 변수 사용
-const ranNoVa = randomNum()
+const ranNoVa = randomNum();
 
 console.log(ranNoVa);
 
-form.addEventListener('submit', e => {
-  const numberLi = document.createElement('li');
+form.addEventListener("submit", e => {
+  const numberLi = document.createElement("li");
   let inputAll = input1.value + input2.value + input3.value;
   numberLi.textContent = inputAll;
   form.appendChild(numberLi);
@@ -27,15 +39,9 @@ form.addEventListener('submit', e => {
   e.target.reset();
 
   // 랜덤숫자와 입력숫자 비교
-  if(inputAll === ranNoVa) { 
-    console.log('hello')
+  if (inputAll === ranNoVa) {
+    console.log("hello");
   } else {
-    console.log('bye')
+    console.log("bye");
   }
-})
-
-
-
-
-
-
+});
